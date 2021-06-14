c09e8c40b952f1d23a40644da06c734ba5a84dd3
Kevin Buchik <kbuchik@gmail.com>
2021-06-14 13:41:01 -0700

    touch control

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ let myGamePiece;
 let torpedoe;
 let obstacle;
 let enemyShip;
+let explosion;
 let enemies = [];
 let obstacles = [];
+let waveTwo = [];
 let bullets = 0;
 let score = 0;
 let newEnemies;
+let rateOfFire = 200.00;
+let bombAmmo = 0;
+
+
 function component(width, height, source, x, y, type) {
     this.type = type;
     if (type == "image") {
@@ -52,44 +58,42 @@ function component(width, height, source, x, y, type) {
         return crash;
     }
 
-
-
-
-
-
-
-
-
-
 }
 
 
 //generate all game pieces that are not randomly generated 
 function startGame() {
     myGameArea.begin();
-    let mathRandom = Math.random() * 480 - 0
+    document.getElementById("opening-scroll").innerHTML = ""
+    document.getElementById("begin").style.display="none"
+    document.getElementById("d-pad").style.display="inherit"
+
 
     // audioObj = new Audio("assets/theme.m4a");
     // audioObj.addEventListener("canplaythrough", event => {
     //     event.preventDefault();
     //     /* the audio is now playable; play it if permissions allow */
-    //     audioObj.play();
+    //     audioObj.play();f
     // });
     myGamePiece = new component(15, 20, "spaceship.png", 150, 240, "image");
     torpedoe = new component(5, 5, "bullet.png", 150, 240, "image");
     obstacle = new component(100, 20, "green", 380, -40);
-    
+    powerUp = new component(50, 50, "green", 300, -40);
+    bomb = new component(10, 10, "blue", 150, 240);
+    explosion = new component(200, 200, "explosion.png", 20, 300, "image");
+
 }
 //game area, canvas context, and mechanism for arrow key movement
 let myGameArea = {
     canvas: document.createElement("canvas"),
     begin: function () {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+
+        this.canvas.width = 480.00;
+        this.canvas.height = 270.00;
         score = 0;
         document.getElementById("show-score").innerHTML = score;
 
-        this.context = this.canvas.getContext("2d");
+        this.context = this.canvas.getContext("2d", { alpha: false });
         this.frameNo = 0;
 
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
@@ -97,17 +101,17 @@ let myGameArea = {
         this.interval = setInterval(updateGameArea, 20);
 
 
-        window.addEventListener('keydown', function (e) {
-            myGameArea.key = e.keyCode;
-            move(e);
+        window.addEventListener('keydown', function (event) {
+            myGameArea.key = event.keyCode;
+            move(event);
         });
-        window.addEventListener('keyup', function (e) {
+        window.addEventListener('keyup', function (event) {
             myGameArea.key = false;
-            clearmove(e);
+            clearmove(event);
         });
-        window.addEventListener('touchmove', function (e) {
-            myGameArea.x = e.touches[0].screenX;
-            myGameArea.y = e.touches[0].screenY;
+        window.addEventListener('touchmove', function (event) {
+            myGameArea.x = event.touches[0].screenX;
+            myGameArea.y = event.touches[0].screenY;
         })
 
     },
@@ -127,6 +131,13 @@ let myGameArea = {
 //game events
 function updateGameArea() {
     var x, y, height, minHeight, maxHeight;
+
+    if (myGameArea.key && myGameArea.key == 37) { myGamePiece.speedX = -4; };
+    if (myGameArea.key && myGameArea.key == 39) { myGamePiece.speedX = 4; };
+    if (myGameArea.key && myGameArea.key == 38) { myGamePiece.speedY = -4; };
+    if (myGameArea.key && myGameArea.key == 40) { myGamePiece.speedY = 4; };
+
+
     for (i = 0; i < obstacles.length; i++) {
         if (myGamePiece.collision(obstacles[i])) {
 
@@ -135,6 +146,11 @@ function updateGameArea() {
             myGameArea.stop();
             console.log("collision");
             return;
+        }
+        if (torpedoe.collision(obstacles[i])) {
+            torpedoe.y = 10000;
+            torpedoe.update()
+
         }
     }
     for (i = 0; i < enemies.length; i++) {
@@ -151,32 +167,36 @@ function updateGameArea() {
         };
         if (torpedoe.collision(enemies[i])) {
             enemies[i].image.src = "explosion.png";
+            score++;
 
             newEnemies = enemies[i]
-            setTimeout(function () {
-                score++;
+            setInterval(function () {
                 console.log("direct hit");
 
                 document.getElementById("show-score").innerHTML = score;
-                newEnemies.height = 0;
-                newEnemies.width = 0;
-                return; 
+                newEnemies.x = 1000;
+                return;
             }, 200);
 
         };
 
+        if (bomb.collision(enemies[i])) {
+            enemies[i].image.src = "explosion.png";
+            score++;
 
+            newEnemies = enemies[i]
+            setInterval(function () {
+                console.log("direct hit");
+
+                document.getElementById("show-score").innerHTML = score;
+                newEnemies.x = 1000;
+                return;
+            }, 200);        
+        };
     }
 
     myGameArea.clear();
     myGameArea.frameNo += 1;
-
-    if (myGameArea.key && myGameArea.key == 37) { myGamePiece.speedX = -4; };
-    if (myGameArea.key && myGameArea.key == 39) { myGamePiece.speedX = 4; };
-    if (myGameArea.key && myGameArea.key == 38) { myGamePiece.speedY = -4; };
-    if (myGameArea.key && myGameArea.key == 40) { myGamePiece.speedY = 4; };
-
-
 
     myGamePiece.newPos();
     myGamePiece.update();
@@ -184,13 +204,38 @@ function updateGameArea() {
 
     torpedoe.x = myGamePiece.x;
     // torpedoe.y = myGamePiece.y;
-    myGamePiece.update();
 
-    torpedoe.update();
 
     torpedoe.width = 2;
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
+    if (myGamePiece.collision(powerUp)) {
+        powerUp.x = 1000;
+        bomb.x = myGamePiece.x;
+        bombAmmo += 5;
+        bomb.update();
+        myGamePiece.update();
+    }
+    else if (myGameArea.key && myGameArea.key == 66 && bombAmmo > 0) {
+
+        obstacle.newPos();
+        obstacle.update();
+        obstacle.y += 1;
+
+        bomb.y -= 100;
+
+        bomb.width = 5;
+        bombAmmo -= 1;
+        console.log(bombAmmo)
+        bomb.update()
+
+        setTimeout(function () {
+            bomb.y = myGamePiece.y
+            bomb.x = myGamePiece.x;
+            bomb.update();
+        }, rateOfFire);
+
+    }
 
     if (obstacle.collision(myGamePiece)) {
         myGamePiece.image.src = "explosion.png";
@@ -205,86 +250,94 @@ function updateGameArea() {
 
         obstacle.newPos();
         obstacle.update();
-        obstacle.y += 1;
+        obstacle.y += 1.0;
 
-        torpedoe.y -= 20
+        torpedoe.y -= 30.00
 
-        torpedoe.width = 5
+        torpedoe.width = 5.0
         torpedoe.update()
 
         setTimeout(function () {
             torpedoe.y = myGamePiece.y
             torpedoe.x = myGamePiece.x;
-        }, 200);
+        }, rateOfFire);
 
     }
 
     else {
-
-        obstacle.y += 1;
+        obstacle.y += 1.0;
         obstacle.update();
-
         myGamePiece.newPos();
         myGamePiece.update();
 
+        setInterval(function () {
+            powerUp.y += 1.0;
+            powerUp.src = "white"
+            powerUp.update();
+
+            return;
+        }, 200);
     }
-    if (myGameArea.frameNo == 1 || everyinterval(70 - score)) {
+    //enemy ship generation
+    if (myGameArea.frameNo == 1 || everyinterval(70)) {
 
         y = myGameArea.canvas.height;
         x = myGameArea.canvas.height;
 
-        minHeight = 200;
-        maxHeight = 600;
-        // function component(width, height, source, x, y, type) {
-
-        // height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-        // width = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
         let mathRandom = Math.random() * 480 - 0
-        enemies.push(new component(30, 30, "ENEMY.png", mathRandom, 0, "image"));
-        // enemyShip.onload = function(){
+        enemies.push(new component(30.02, 30.02, "ENEMY.png", mathRandom, 0, "image"));
         console.log(enemies)
-
-        // }
+        for (i = 0; i < enemies.length; i++) {
+            enemies[i].y += 1;
+            enemies[i].update();
+        };
 
     }
-
+    //obstacle generation 
     if (myGameArea.frameNo == 1 || everyinterval(120 - score)) {
 
         y = myGameArea.canvas.height;
         x = myGameArea.canvas.height;
-        minHeight = 200;
-        maxHeight = 600;
+
         let mathRandom = Math.random() * 480 - 0
 
-        obstacles.push(new component(100, 20, "red", mathRandom, 0));
+        obstacles.push(new component(100.01, 20.01, "red", mathRandom, 0));
 
-    }
+    };
 
-
+//enemy speed
     for (i = 0; i < enemies.length; i++) {
         enemies[i].y += 1;
         enemies[i].update();
-    }
+    };
+
     for (i = 0; i < obstacles.length; i++) {
         obstacles[i].y += 1;
         obstacles[i].update();
-    }
+    };
 
+    // if (score > 2 && (myGameArea.frameNo == 1 || everyinterval(120 - score))){
+    //     let mathRandom = Math.random() * 480 - 0
 
-    //     if (score == 2) {
-    // myGameArea.begin()    }
+    //     waveTwo.push(new component(30, 30, "green", mathRandom, 0, "color"));
+    //     for (i = 0; i < waveTwo.length; i++) {
+    //         waveTwo[i].y += 1;
+    //         waveTwo[i].update();
+    //     };
+    // }
 
 }
 
 function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
+    if ((myGameArea.frameNo / n) % 1 == 0) { return true; };
     return false;
 }
 
 function move(dir) {
     myGamePiece.image.src = "spaceshipburst.png";
-    torpedoe.x = myGamePiece.x;
-    torpedoe.y = myGamePiece.y;
+
+    bomb.x = myGamePiece.x;
+    bomb.y = myGamePiece.y;
 
     if (dir == "up") { myGamePiece.speedY = -1; };
     if (dir == "down") { myGamePiece.speedY = 1; };
@@ -297,7 +350,16 @@ function clearmove() {
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
 }
-// stars
+
+
+window.onload = () => {    
+    typeWriter(txt)
+
+
+}
+//opening scroll
+
+// // stars
 // window.onload = () => {
 //     const $el = document.body;
 
@@ -307,43 +369,43 @@ function clearmove() {
 //         return Math.random() * (max - min) + min;
 //     }
 
-    // Generate a star <div>
+//     // Generate a star 
 
-    // const genStar = () => {
+//     const genStar = () => {
 
-    //     const star = document.createElement("img");
-    //     star.src = ("assets/stars.png")
-    //     star.classList.add("star");
+//         const star = document.createElement("img");
+//         star.src = ("assets/stars.png")
+//         star.classList.add("star");
 
-    //     // Gen star coordinates relative to $el size
-    //     let x = genRandomNumber(1, $el.offsetWidth);
-    //     let y = genRandomNumber(1, $el.offsetHeight);
+//         // Gen star coordinates relative to $el size
+//         let x = genRandomNumber(1, $el.offsetWidth);
+//         let y = genRandomNumber(1, $el.offsetHeight);
 
-    //     const { style } = star;
+//         const { style } = star;
 
-    //     style.left = Math.floor(x) + "px";
-    //     style.top = Math.floor(y) + "px";
+//         style.left = Math.floor(x) + "px";
+//         style.top = Math.floor(y) + "px";
 
-    //     style.setProperty(
-    //         "--star-size",
-    //         genRandomNumber(1, 4) + "px"
-    //     );
+//         style.setProperty(
+//             "--star-size",
+//             genRandomNumber(1, 4) + "px"
+//         );
 
-    //     style.setProperty(
-    //         "--twinkle-duration",
-    //         Math.ceil(genRandomNumber(1, 5)) + "s"
-    //     );
+//         style.setProperty(
+//             "--twinkle-duration",
+//             Math.ceil(genRandomNumber(1, 5)) + "s"
+//         );
 
-    //     style.setProperty(
-    //         "--twinkle-delay",
-    //         Math.ceil(genRandomNumber(1, 5)) + "s"
-    //     );
+//         style.setProperty(
+//             "--twinkle-delay",
+//             Math.ceil(genRandomNumber(1, 5)) + "s"
+//         );
 
-    //     return star;
-    // }
+//         return star;
+//     }
 
-    // for (let index = 0; index < 25; index++) {
-    //     $el.append(genStar());
-    // }
+//     for (let index = 0; index < 25; index++) {
+//         $el.append(genStar());
+//     }
 
 // }

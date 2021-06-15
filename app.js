802b7de159bfeaca9e04cd8ff9b468144debd9df
Kevin Buchik <kbuchik@gmail.com>
2021-06-14 17:15:34 -0700

    remove zoom ios

--- app.js
+++ app.js
@@ -392,6 +392,16 @@ function shootTorpedoe(){
 }
 
 
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
+
 //     const genRandomNumber = (min, max) => {
 //         return Math.random() * (max - min) + min;
 //     }
